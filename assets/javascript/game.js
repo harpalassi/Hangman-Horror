d2b96348f70dfa94e306982c5b71d33a95001f69
Speedway <harpalassi@gmail.com>
2018-10-15 19:54:36 -0500

    skeleton

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+                     //game variables
+//word bank
+var words = ["halloween", "scream", "suspiria", "carrie", "psycho", "hellraiser"];
+//random word from bank
+var word = words[Math.floor(Math.random() * words.length)];
+//answer array
+var answer = [];
+
+var wins;
+var guessedLetters;
+var guessesLeft = word.length;
+
+
+for (var i = 0; i < word.length; i++) {
+    answer[i] = "_";
+}
+
+
+document.querySelector("#wins").innerHTML =  wins;
+document.querySelector("#currentWord").innerHTML = word;
+document.querySelector("#guessesLeft").innerHTML = guessesLeft;
+document.querySelector("#lettersGuessed").innerHTML += guessedLetters;
