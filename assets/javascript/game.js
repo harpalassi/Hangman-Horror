f899c1d25e6ee300315bc5271a734420340a3e68
Speedway <harpalassi@gmail.com>
2018-10-18 15:48:28 -0500

    more game mechanics

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,26 @@
-                     //game variables
 //word bank
 var words = ["halloween", "scream", "suspiria", "carrie", "psycho", "hellraiser"];
 //random word from bank
 var word = words[Math.floor(Math.random() * words.length)];
-//empty answer array
-var underscores = [];
-// variables
+//answer array
+var answer = [];
+
 var wins = 0;
 var guessedLetters = [];
 var guessesLeft = 12;
 
 console.log(word);
 
-//creates underscores for length of random word
-
-function newWord () {
+document.onkeyup = function(event) {
     for (var i = 0; i < word.length; i++) {
-        underscores.push("_")
+        answer.push("_");
+        
     }
-    return underscores; 
-}
-
-console.log(newWord());
-
-//get the users guesses
-document.addEventListener("keypress", function(event) {
-    var keyword = String.fromCharCode(event.keyCode);
-
-    //if users guess is right
-    if (word.indexOf(keyword) > -1 ) {
-        console.log(true);
-    }
-});
     
+// DOM
+document.querySelector("#wins").innerHTML = wins;
+document.querySelector("#currentWord").innerHTML = answer.join(" ");
+document.querySelector("#guessesLeft").innerHTML = guessesLeft;
+document.querySelector("#lettersGuessed").innerHTML += guessedLetters;
 
-
-
-    
-
-// document.querySelector("#wins").innerHTML =  wins;
-// document.querySelector("#currentWord").innerHTML = underscores;
-// document.querySelector("#guessesLeft").innerHTML = guessesLeft;
-// document.querySelector("#lettersGuessed").innerHTML += guessedLetters;
-
+};
