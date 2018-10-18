447b011fac144bf7d388a8c70f36a93ee4e4bd36
Speedway <harpalassi@gmail.com>
2018-10-18 18:30:10 -0500

    working game mechanics

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //word bank
-var words = ["halloween", "scream", "suspiria", "carrie", "psycho", "hellraiser"];
+var words = ["halloween", "scream", "suspiria", "carrie", "hellraiser"];
 //random word from bank
 var word = words[Math.floor(Math.random() * words.length)];
 //answer array
@@ -11,6 +11,7 @@ var guessesLeft = 12;
 
 console.log(word);
 
+//run this function when page start to load underscores
 document.addEventListener("DOMContentLoaded", function(event) { 
 
 
@@ -18,16 +19,51 @@ document.addEventListener("DOMContentLoaded", function(event) {
         answer.push("_");
         
     }
-
+//run this when user presses a key to log the key. 
     document.onkeyup = function(event) {
         var userGuess = event.key;
-        console.log(event.key);
+        
+
+//check if userGuess is in the word.
+    var letterFound = false;
+    for (var i = 0; i < word.length; i++) {
+   
+     if (word[i] === userGuess) {
+        answer[i] = word[i];
+        letterFound = true;
+        
+    }
+
+
+    //   console.log(userGuess);      
+     } 
+
+
+    if (letterFound === false) {
+        guessedLetters.push(userGuess);
+    }
+        
+
+
+
+
+     document.querySelector("#currentWord").innerHTML = answer.join(" ");
+     document.querySelector("#lettersGuessed").innerHTML = guessedLetters.join(", ");
+
+
+
+   //check if userGuess is not in word. 
+//   if (word.indexOf(userGuess) < -1) {
+//        guessedLetters.push(userGuess);
+    
+   
+   
 }    
     
 // DOM
 document.querySelector("#wins").innerHTML = wins;
 document.querySelector("#currentWord").innerHTML = answer.join(" ");
 document.querySelector("#guessesLeft").innerHTML = guessesLeft;
-document.querySelector("#lettersGuessed").innerHTML += guessedLetters;
+document.querySelector("#lettersGuessed").innerHTML = guessedLetters.join(", ");
 
 });
