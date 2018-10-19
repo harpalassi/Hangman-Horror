1858d9c3122386af3e5a27621c27542237bc4863
Speedway <harpalassi@gmail.com>
2018-10-18 22:10:21 -0500

    reset game completed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,56 @@
-//word bank
-var words = ["halloween", "scream", "suspiria", "carrie", "hellraiser"];
-//random word from bank
-var word = words[Math.floor(Math.random() * words.length)];
-//answer array
-var answer = [];
 
-var wins = 0;
+var words = ["halloween", "scream", "suspiria", "carrie", "hellraiser"];
+var word;
+var answer;
+var wins;
 var guessedLetters = [];
-var guessesLeft = 12;
+var guessesLeft;
+//run this function when page start to load underscores
+
+function newGame(event) {
+
+//word bank
+
+//random word from bank
+word = words[Math.floor(Math.random() * words.length)];
+//answer array
+answer = [];
+
+wins = 0;
+guessedLetters = [];
+guessesLeft = 12;
 
 console.log(word);
-
-//run this function when page start to load underscores
-document.addEventListener("DOMContentLoaded", function(event) { 
-
-
     for (var i = 0; i < word.length; i++) {
         answer.push("_");
-        
-    }
-//run this when user presses a key to log the key. 
-    document.onkeyup = function(event) {
-        var userGuess = event.key;
-        
 
-//check if userGuess is in the word.
+    }
+    //run this when user presses a key to log the key. 
+    
+    // DOM
+    document.querySelector("#wins").innerHTML = wins;
+    document.querySelector("#currentWord").innerHTML = answer.join(" ");
+    document.querySelector("#guessesLeft").innerHTML = guessesLeft;
+    document.querySelector("#lettersGuessed").innerHTML = guessedLetters.join(", ");
+
+
+}
+
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+
+
+    //check if userGuess is in the word.
     var letterFound = false;
     for (var i = 0; i < word.length; i++) {
-   
-     if (word[i] === userGuess) {
-        answer[i] = word[i];
-        letterFound = true;
-        
-    }
 
-    //   console.log(userGuess);      
-     } 
+        if (word[i] === userGuess) {
+            answer[i] = word[i];
+            letterFound = true;
+
+        }
+
+    }
 
 
     if (letterFound === false) {
@@ -45,29 +60,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     if (guessesLeft < 0) {
         alert("You Lose");
+        guessesLeft = 12;
+        newGame();
     }
-        
 
-     document.querySelector("#currentWord").innerHTML = answer.join(" ");
-     document.querySelector("#lettersGuessed").innerHTML = guessedLetters.join(", ");
-     document.querySelector("#guessesLeft").innerHTML = guessesLeft;
-     document.querySelector("#wins").innerHTML = wins;
+    if (answer.join('') === word) {
+        wins++;
+        alert("You Win")
+        newGame();
+    }
+
+    document.querySelector("#currentWord").innerHTML = answer.join(" ");
+    document.querySelector("#lettersGuessed").innerHTML = guessedLetters.join(", ");
+    document.querySelector("#guessesLeft").innerHTML = guessesLeft;
+    document.querySelector("#wins").innerHTML = wins;
 
 
 
 
-   //check if userGuess is not in word. 
-//   if (word.indexOf(userGuess) < -1) {
-//        guessedLetters.push(userGuess);
-    
-   
-   
-}    
-    
-// DOM
-document.querySelector("#wins").innerHTML = wins;
-document.querySelector("#currentWord").innerHTML = answer.join(" ");
-document.querySelector("#guessesLeft").innerHTML = guessesLeft;
-document.querySelector("#lettersGuessed").innerHTML = guessedLetters.join(", ");
+    //check if userGuess is not in word. 
+    //   if (word.indexOf(userGuess) < -1) {
+    //        guessedLetters.push(userGuess);
 
-});
+
+
+}
+
+
+document.addEventListener("DOMContentLoaded", newGame);
